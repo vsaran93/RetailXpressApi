f7404709064f03e6b00698fc0e4a86d099c78d2c
vsaran93 <vishnusaran37@gmail.com>
2025-05-04 23:58:17 +0100

    added ef core nuget pack to domain

--- src/core/Domain/Common/BaseEntity.cs
+++ src/core/Domain/Common/BaseEntity.cs
@@ -1,3 +1,17 @@
-public class BaseEntity 
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace Domain.Common
 {
+    public class BaseEntity
+    {
+        [Key]
+        public int Id { get; set;}
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set;}
+    }
 }
